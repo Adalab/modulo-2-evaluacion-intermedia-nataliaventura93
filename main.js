9323edbd9730c3dc46ff962c37810257770d82d5
nataliaventura93 <nataliaventura93@gmail.com>
2023-05-31 13:05:48 +0200

    Add solution2

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 'use strict'
 
-let selectNumber = document.querySelector('.js_number');
-let selectedBet = document.querySelector('.js_inputBet');
-let btnPlay = document.querySelector('.js_btnPlay');
-let message = document.querySelector('.js_message');
-let balance = document.querySelector('.js_balance');
-let saldo = 50;
-
+const selectNumber = document.querySelector('.js_number');
+const selectedBet = document.querySelector('.js_inputBet');
+const btnPlay = document.querySelector('.js_btnPlay');
+const message = document.querySelector('.js_message');
+const balance = document.querySelector('.js_balance');
+let saldo = 50;                      
+       
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
@@ -14,25 +14,27 @@ function getRandomNumber(max) {
 
 
 
-btnPlay.addEventListener('click', (event) => {
+ const handleClickButton = (event) => {
     event.preventDefault()
-    let randomNumber = getRandomNumber(6)
+    const randomNumber = getRandomNumber(6)
     console.log(randomNumber)
     if (parseInt(selectNumber.value) === randomNumber){
         message.innerHTML = "Has ganado el doble de lo apostado"
-        saldo = saldo + selectedBet.value*2
+        saldo = saldo + parseFloat(selectedBet.value)*2
         balance.innerHTML = `Saldo: ${saldo}`
     }
     else {
         message.innerHTML = "Has perdido lo apostado"
-        saldo = saldo - selectedBet.value
+        saldo = saldo - parseFloat(selectedBet.value)
         balance.innerHTML = `Saldo: ${saldo}`
         
     }
     if(saldo<selectedBet.input){
         message.innerHTML = "No tienes suficiente saldo para apostar"
     }
-});
+}
+
+btnPlay.addEventListener('click', handleClickButton);
 
 
 
